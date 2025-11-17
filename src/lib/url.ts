dd6d6d1a7b38977b43d5ae71d2b107f6d06f1a3d
Muhammad-Naveed704 <epicplayz.147@gmail.com>
2025-11-17 14:37:10 +0500

    fix url

--- src/lib/url.ts
+++ src/lib/url.ts
@@ -4,10 +4,29 @@ export function assetUrl(path: string | undefined | null) {
   
   // Default to localhost:4000 if no environment variable is set
   const base = process.env.NEXT_PUBLIC_API_BASE || 'http://localhost:8000';
+
   const cleanBase = base.replace(/\/?api\/?$/, '');
+  console.log('cleanBase:', cleanBase);
   
   if (!cleanBase) return path;
-  return path.startsWith('/') ? `${cleanBase}${path}` : `${cleanBase}/${path}`;
-}
+  let newstr =path.startsWith('/') ? `${cleanBase}${path}` : `${cleanBase}/${path}`;
+  console.log('new str:', newstr );
+  
+    return newstr;
+  }
+
+
+
+//   export function assetUrl(path: string | undefined | null) {
+//   if (!path) return '';
+//   if (path.startsWith('http://') || path.startsWith('https://')) return path;
+  
+//   // Default to localhost:4000 if no environment variable is set
+//   const base = process.env.NEXT_PUBLIC_API_BASE || 'http://localhost:8000';
+//   const cleanBase = base.replace(/\/?api\/?$/, '');
+  
+//   if (!cleanBase) return path;
+//   return path.startsWith('/') ? `${cleanBase}${path}` : `${cleanBase}/${path}`;
+// }
 
 
